4d4d1eb88c28f68acc98688aaffa21f811e957cc
rodrigoazv <rodrigozcma@gmail.com>
2019-02-26 14:24:47 -0300

    Adding selectedPrefixText , term for question

--- index.py
+++ index.py
@@ -3,9 +3,16 @@ def start():
 
     def askAndReturnSearchTerm():
         return input('Type your search term : ') 
-    
     content["searchTerm"] = askAndReturnSearchTerm()
 
+
+    def askAndReturnSearchTerm():
+        prefixes = ('Who i', 'Who are', 'Who')
+        for i in range(len(prefixes)):
+            print( prefixes[i-1] )
+        selectPrefix = int(input('Choose your search: '))
+        return prefixes[selectPrefix]
+    content["selectedPrefixText"] = askAndReturnSearchTerm()
     print(content)
 
 
